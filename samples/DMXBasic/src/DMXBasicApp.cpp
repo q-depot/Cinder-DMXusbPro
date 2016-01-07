2f55b54e43a88fde5f5e2fdb4ba4186d9dd0d8b0
Boston Conference Room <team@sosolimited.com>
2016-01-07 17:40:20 -0500

    List devices in sample and interact with drag.
    Applying settings doesn't seem to actually change the framerate.
    Querying the settings is probably working.

--- samples/DMXBasic/src/DMXBasicApp.cpp
+++ samples/DMXBasic/src/DMXBasicApp.cpp
@@ -12,7 +12,7 @@ using namespace std;
 class DMXBasicApp : public App {
 public:
 	void setup() override;
-	void mouseDown(MouseEvent event) override;
+	void mouseDrag(MouseEvent event) override;
 	void keyDown(KeyEvent event) override;
 	void update() override;
 	void draw() override;
@@ -24,18 +24,26 @@ private:
 
 void DMXBasicApp::setup()
 {
+    auto devices = Serial::getDevices();
+    for (auto &device: devices) {
+        console() << device.getName() << " at " << device.getPath() << endl;
+    }
 	// Connect to enttec device and run data loop at 30 fps.
-	_device = std::make_shared<dmx::EnttecDevice>("tty.usbserial-ENWER12L", 30);
+	_device = std::make_shared<dmx::EnttecDevice>("tty.usbserial-EN", 30);
 	// Load the device settings synchronously.
 	_settings = _device->loadSettings().get();
 	console() << _settings << endl;
+    _settings.device_fps = 30;
+    _device->applySettings(_settings);
+//    _settings = _device->loadSettings().get();
+//    console() << "after change: " << _settings << endl;
 }
 
-void DMXBasicApp::mouseDown( MouseEvent event )
+void DMXBasicApp::mouseDrag( MouseEvent event )
 {
-	auto channel = mix<size_t>(0, _buffer.size(), (float)event.getPos().x / getWindowWidth());
-	auto value = mix(0, 255, (float)event.getPos().y / getWindowHeight());
-	_buffer.setChannelValues(channel, Color8u(value, value, value));
+    auto channel = mix<size_t>(0, _buffer.size(), (float)event.getPos().x / getWindowWidth());
+    auto value = mix(0, 255, (float)event.getPos().y / getWindowHeight());
+    _buffer.setChannelValue(channel, value);
 }
 
 void DMXBasicApp::keyDown(cinder::app::KeyEvent event)
