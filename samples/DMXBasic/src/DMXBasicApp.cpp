a9de98dbd194fcaad98dfad0afe8903ea378a6ad
David Wicks <david@sosolimited.com>
2016-01-08 10:46:49 -0500

    Minor whitespace (test machine had different settings).

--- samples/DMXBasic/src/DMXBasicApp.cpp
+++ samples/DMXBasic/src/DMXBasicApp.cpp
@@ -22,37 +22,40 @@ private:
 
 void DMXBasicApp::setup()
 {
-    auto devices = Serial::getDevices();
-    for (auto &device: devices) {
-        console() << device.getName() << " at " << device.getPath() << endl;
-    }
+	auto devices = Serial::getDevices();
+	for (auto &device: devices) {
+		console() << device.getName() << " at " << device.getPath() << endl;
+	}
 	// Connect to enttec device and run data loop at 30 fps.
 	_device = std::make_shared<dmx::EnttecDevice>("tty.usbserial-EN", 30);
 	// Load the device settings synchronously.
 	_settings = _device->loadSettings().get();
 	console() << _settings << endl;
 
-    // As of now, it is unclear whether applying the settings works as expected.
 	// Start sending data to device on an interval in a secondary thread.
 	_device->startLoop();
+
+	// As of now, it is unclear whether applying the settings works as expected.
 //    _settings.device_fps = 30;
 //    _device->applySettings(_settings);
 }
 
 void DMXBasicApp::update()
 {
-    auto t = fract(getElapsedSeconds());
-    for (auto c = 0; c <= 512 - 3; c += 3) {
-        auto hue = fract(t + (c / 512.0f));
-        auto color = Color(CM_HSV, hue, 0.5, 0.5);
-        _buffer.setChannelValues(c, color);
-    }
+	auto t = fract(getElapsedSeconds());
+	for (auto c = 0; c <= 512 - 3; c += 3) {
+		auto hue = fract(t + (c / 512.0f));
+		auto color = Color(CM_HSV, hue, 0.5, 0.5);
+		_buffer.setChannelValues(c, color);
+	}
+
+	// Update the data to send to the DMX device.
 	_device->bufferData(_buffer);
 }
 
 void DMXBasicApp::draw()
 {
-	gl::clear( Color( 0, 0, 0 ) ); 
+	gl::clear( Color( 0, 0, 0 ) );
 }
 
 CINDER_APP( DMXBasicApp, RendererGl )
